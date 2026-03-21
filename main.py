85df88eab4b1463d1080a68dfa202f07c5a441f0
Fahim <fahimshahariar.fs@gmail.com>
2026-03-21 22:43:53 +0600

    Basic chatbot

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from google import genai
 
 
-client = genai.Client()
+client = genai.Client(api_key="YOUR_API_KEY")
 
 
-response = genai.models.generate_content(
-    model="models/gemini-3-flash-preview", contents = "Explain how one can use the Google Gemini API to create a chatbot about all the info of Bangladesh.")
-print(response)
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Give me step by step instructions how can I create a chatbot using Python and the Gemini API. The chatbot will only know about Bangladesh and will be used to answer questions about Bangladesh."
+)
+print(response.text)
