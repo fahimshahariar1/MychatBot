e2cc4ed4b7d50337d12c6880c33c905eccad18a0
Fahim <fahimshahariar.fs@gmail.com>
2026-03-24 21:58:34 +0600

    BengalBot

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from google import genai
-import os
+from google.genai import types
 
 
 client = genai.Client(api_key="")
 
 system_instructions =  ("You are a specialized chatbot known as 'BengalBot'. "
-    "Your knowledge is strictly limited to Bangladesh. " "You only reply in Bangla language."
+    "Your knowledge is strictly limited to Bangladesh. " "You only reply in Banglish language."
     "You can discuss its history, culture, geography, food, politics, sports, and economy. "
     "If a user asks a question that is NOT about Bangladesh, you must politely decline "
     "and say: 'I am a specialist on Bangladesh only. Please ask me anything related to Bangladesh!'")
 
-model = genai.GenerativeModel(
-    model_name="gemini-3-flash-preview", system_instructions=system_instructions
-)
 
-chat = model.start_chat(history=[])
+chat = client.chats.create(
+    model="gemini-2.5-flash", # gemini-3-flash-preview might require specific tier access
+    config= types.GenerateContentConfig(
+        system_instruction=system_instructions
+    )
+)
 
 
 print("Welcome to BengalBot! Ask me anything about Bangladesh.")
@@ -25,7 +27,5 @@ while True:
     if user_input.lower() == "exit":
         print("BengalBot: Goodbye! Have a great day!")
         break
-
-    chat.add_user_message(user_input)
-    response = chat.generate_response()
+    response = chat.send_message(user_input)
     print(f"BengalBot: {response.text}")
